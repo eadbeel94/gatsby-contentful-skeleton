033c24e8356ba8b039a3f024a8468cd5c72656d7
eadbeel94 <eadbeel94@outlook.com>
2022-05-05 16:30:54 -0500

    add gatsby source contentful plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,14 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    //{
+    //  resolve: `gatsby-source-contentful`,
+    //  options: {
+    //    spaceId: process.env.GATSBY_CONTENTFUL_SPACE_ID,
+    //    // Learn about environment variables: https://gatsby.dev/env-vars
+    //    accessToken: process.env.GATSBY_CONTENTFUL_ACCESS_TOKEN,
+    //  },
+    //},
+  ],
 }
